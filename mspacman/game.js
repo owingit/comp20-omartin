7e652d929105a61083b8c6f1d5476b1a41ffabaf
Owen Martin <martin.owen22@gmail.com>
2015-03-02 15:49:54 -0500

    Messing around with drawImage parameters

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,21 @@
-// Replace this with your code...
+function init() {
+	draw();
+}
+
+function draw() {
+    var canvas = document.getElementById('game_canvas');
+        
+    // Check if canvas is supported on browser
+    if (canvas.getContext) {
+        var ctx = document.getElementById('game_canvas').getContext('2d');
+	    var img = new Image();
+	    img.onload = function(){
+	      ctx.drawImage(img,320,0,600,125,0,0,600,130);
+  		};
+  		img.src = 'pacman10-hp-sprite.png';
+    }
+
+    else {
+        alert('Sorry, canvas is not supported on your browser!');
+	}
+}
