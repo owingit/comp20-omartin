3543ef7c7b1fcad3d1543fe245235b3cc8b43c2f
Owen Martin <martin.owen22@gmail.com>
2015-03-10 15:33:48 -0400

    Editing lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,17 @@
-// Your JavaScript goes here...
+function parse() {
+	var req = new XMLHttpRequest();
+
+	req.open("get", "data.json", true);
+
+	req.onreadystatechange = function() {
+		if (req.readyState == 4 && req.status == 200) {
+			var vals = JSON.parse(req.responseText);
+			var result = "";
+        	for (i = 0; i < vals.length; i++) {
+					result += "<p>" + vals[i]['content'] + " - " + vals[i]['username'] + "</p>";
+			}
+        	document.getElementById("messages").innerHTML = result;
+    	}
+   }
+   req.send();
+}
